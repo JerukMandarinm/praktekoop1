d74f42002ff601c7ae5d7bb98369441b3ccba8fe
JerukMandarinm <shalwanafiisa@gmail.com>
2025-08-29 12:25:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <title>Tugas PPBO</title>
   </head>
   <body>
-    <h1>hi, selamat datang di tugas praktikum OOP 1</h1>
+    <h1>hi, saya suka warna pink </h1>
     <?php echo '<p>coba pencet</p>'; ?> 
     <a href="home.php">Home</a>
   </body>
